e1a72bb74000448f29696dde0d039f1dcf94df42
kkonii <119216472+kkonii@users.noreply.github.com>
2023-07-07 23:01:17 +0900

    프로그래머스/lv0/문자열 출력하기

---
+++ Solution/src/programmers/OutputString.java
@@ -0,0 +1,13 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class OutputString {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String a = sc.next();
+        
+        System.out.print(a);
+	}
+}
