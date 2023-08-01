5f0e0b1971f6bfc52b032a5b5e2664cdc7e8132b
kkonii <119216472+kkonii@users.noreply.github.com>
2023-08-01 23:12:04 +0900

    백준/3003번/킹퀸룩비숍나이트폰

--- Solution/src/beakjune/Beakjune_3003.java
+++ Solution/src/beakjune/Beakjune_3003.java
@@ -3,7 +3,7 @@ package beakjune;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class beakjune_3003 {
+public class Beakjune_3003 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
