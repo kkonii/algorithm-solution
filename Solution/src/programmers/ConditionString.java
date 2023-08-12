29441e6fa4485831bc7acb0c7aaeff97cf60f37c
kkonii <119216472+kkonii@users.noreply.github.com>
2023-08-12 21:50:21 +0900

    프로그래머스/level.0/조건문자열

---
+++ Solution/src/programmers/ConditionString.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class ConditionString {
+	public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+        
+        if(ineq.equals(">")&&eq.equals("=")){
+            if (n >= m) answer = 1;
+        }
+        if(ineq.equals("<")&&eq.equals("=")){
+            if (n <= m) answer = 1;
+        }
+        if(ineq.equals(">")&&eq.equals("!")){
+            if (n > m) answer = 1;
+        }
+        if(ineq.equals("<")&&eq.equals("!")){
+            if (n < m) answer = 1;
+        }
+        
+        return answer;
+    }
+}
