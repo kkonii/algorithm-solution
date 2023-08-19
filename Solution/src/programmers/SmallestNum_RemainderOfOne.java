0e9b545fd284c9afe7497e1eba364ab5ea1b0d48
kkonii <119216472+kkonii@users.noreply.github.com>
2023-08-19 13:12:06 +0900

    프로그래머스/level.1/나머지가1이되는수찾기

---
+++ Solution/src/programmers/SmallestNum_RemainderOfOne.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class SmallestNum_RemainderOfOne {
+    public int solution(int n) {
+        int answer = 0;
+	        
+	    for(int i=1;i<n;i++) {
+	        if(n % i == 1) {
+	            answer = i; break;
+	        }
+	    }
+	        
+	    return answer;
+    }
+}
